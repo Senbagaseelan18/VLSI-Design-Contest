8e6680dabe7815e7c6f4269174b1765faa1bb8ff
Senbagaseelan V <158827420+Senbagaseelan18@users.noreply.github.com>
2025-11-09 21:06:43 +0530

    Update capture_video.sh

--- USB-Camer-Setup/scripts/capture_video.sh
+++ USB-Camer-Setup/scripts/capture_video.sh
@@ -1,1 +1,7 @@
 
+#!/bin/bash
+DEVICE=/dev/video0
+OUTFILE=/root/video.mjpg
+v4l2-ctl -d $DEVICE --set-fmt-video=width=1920,height=1080,pixelformat=MJPG
+v4l2-ctl -d $DEVICE --stream-mmap --stream-count=300 --stream-to=$OUTFILE
+echo "Video recorded at $OUTFILE"
